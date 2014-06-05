6a686f4d9e940024f426d79201f35653aaa223f6
undoom <undoom@gmail.com>
2014-06-05 11:48:44 +0200

    Notification Native

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 var app = {
 
     findByName: function() {
-        console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
             var l = employees.length;
             var e;
@@ -14,10 +13,19 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
+        this.store = new LocalStorageStore();
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
 
+        this.showAlert('Données initialisées','Info');
+    },
+
+    showAlert: function(message,title){
+        if(navigator.notification){
+            navigator.notification.alert(message, null, title, 'OK');
+        }else{
+            alert(title?(title+" : "+message) : message);
+        }
+    }
 };
 
 app.initialize();
