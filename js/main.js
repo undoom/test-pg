d90d6e4341ac24dc5fed14a9645161dd4b9c0d66
undoom <undoom@gmail.com>
2014-06-05 12:26:18 +0200

    Support APN

--- js/main.js
+++ js/main.js
@@ -19,6 +19,30 @@ var app = {
         //this.showAlert('Données initialisées','Info');
         var self = this;
         $(document).on("click",".add-location-btn",this.addLocation);
+        $(document).on("click",".change-pic-btn",this.changePicture);
+    },
+
+    changePicture:function(event){
+        event.preventDefault();
+        if(!navigator.camera){
+            app.showAlert("Camera non supportée","Erreur");
+            return;
+        }
+        var options =   {
+                            quality:50,
+                            destinationType: Camera.DestinationType.DATA_URL,
+                            sourceType:1,   //0 : Gallerie, 1 : APN, 2 Album
+                            encodingType:0  //0 : JPG, 1 : PNG
+                         }
+        navigator.camera.getPicture(
+            function(imageData){
+                $(this).after("<img src='data:images/jpeg;base64,"+imageData);
+            },function(){
+                app.showAlert('Erreur à la récupération e l\'image', 'Erreur');
+            },
+            options);
+
+        return false;
     },
 
     addLocation:function(event){
